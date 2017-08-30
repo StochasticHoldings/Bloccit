762eb3235a6cc2bba06f1bdb01d6a5ed53fa2c6f
StochasticHome <StochasticHoldings@gmail.com>
2017-08-30 13:25:33 -0400

    got rid of comments in post_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,20 +18,20 @@ class PostsController < ApplicationController
   end
 
   def create
- # #9
+
      @post = Post.new
      @post.title = params[:post][:title]
      @post.body = params[:post][:body]
      @topic = Topic.find(params[:topic_id])
      @post.topic = @topic
 
- # #10
+
      if @post.save
- # #11
+
        flash[:notice] = "Post was saved."
        redirect_to [@topic, @post]
      else
- # #12
+
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :new
      end
@@ -60,7 +60,7 @@ def update
    def destroy
      @post = Post.find(params[:id])
 
- # #8
+
      if @post.destroy
        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
        redirect_to @post.topic
