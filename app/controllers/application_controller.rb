d58f3fd2b1355c92b4a162bc577933842122a78a
Stochastic <Stochasticholdings@gmail.com>
2017-09-27 12:40:37 -0400

    completed assignment 26

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :avatar_url
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
