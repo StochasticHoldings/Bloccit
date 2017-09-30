bca705e9df2ff6306c008cd36bd1dffd41883097
Stochastic <Stochasticholdings@gmail.com>
2017-09-30 15:07:41 -0400

    started checkpoint 27

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,63 +6,72 @@
 # otherwise you would need a big script of SQL or do it by hand.
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
- 15.times do
-   Topic.create!(
-     name:         RandomData.random_sentence,
-     description:  RandomData.random_paragraph
-   )
- end
- topics = Topic.all
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
 
- # Create Posts
- 50.times do
- # #1
-   Post.create!(
-   topic:  topics.sample,
- # #2
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+# Create Posts
+50.times do
+  Post.create!(
+    user:   users.sample,
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = Post.all
 
- # Create Sponsored Posts
- 5.times do
- # #1
-   SponsoredPost.create!(
-   topic:  topics.sample,
- # #2
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = SponsoredPost.all
+# Create Sponsored Posts
+5.times do
+  SponsoredPost.create!(
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = SponsoredPost.all
 
- # Create Comments
- # #3
- 100.times do
-   Comment.create!(
- # #4
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+# Create Comments
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
 
- 50.times do
- # #1
-   Advertisement.create!(
- # #2
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph,
-     price: rand(50),
-   )
- end
+50.times do
+  Advertisement.create!(
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph,
+    price: rand(50),
+  )
+end
 
- Post.find_or_create_by(title: "FooZebra", body: "BarBarBarBarBarBarBarBarBarBarBarBar")
+Post.find_or_create_by(title: "FooZebra", body: "BarBarBarBarBarBarBarBarBarBarBarBar")
 
+user = User.first
+user.update_attributes!(
+  email: 'youremail.com', # replace this with your personal email
+  password: 'helloworld'
+)
 
- puts "Seed finished"
- puts "#{Topic.count} topics created"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
