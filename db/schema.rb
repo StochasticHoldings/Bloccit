cba024830805c33a1909cef0fc1e3ad62867686e
Stochastic <Stochasticholdings@gmail.com>
2017-10-02 20:57:32 -0400

    completed checkpoint-28

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930171005) do
+ActiveRecord::Schema.define(version: 20171003004208) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20170930171005) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -64,12 +64,20 @@ end
 
 Post.find_or_create_by(title: "FooZebra", body: "BarBarBarBarBarBarBarBarBarBarBarBar")
 
-user = User.first
-user.update_attributes!(
- email: 'bob@email.com', # replace this with your personal email
-  password: 'helloworld'
-)
+# Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
